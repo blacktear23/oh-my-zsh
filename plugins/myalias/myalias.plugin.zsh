2b555aa86a7be4fea449c68002bffd2289719e4a
Rain Lee <blacktear23@gmail.com>
2013-08-06 09:05:20 +0800

    For my update

---
+++ plugins/myalias/myalias.plugin.zsh
@@ -0,0 +1,59 @@
+# Misc
+alias ls="ls -G"
+alias ll="ls -lh"
+alias la="ls -al"
+alias work="cd ~/work"
+alias ywork="cd ~/ywork"
+alias research="cd ~/research"
+alias gemdir="cd /opt/local/lib/ruby1.9/gems/1.9.1/gems/"
+alias pydir="cd /Library/Frameworks/Python.framework/Versions/Current/lib/python2.7/site-packages"
+alias works="cd ~/workspace"
+alias res="cd ~/research"
+alias draft="cd ~/Documents/draft"
+alias godir="cd /opt/local/go/src/pkg"
+alias sshpub="cat ~/.ssh/id_rsa.pub"
+alias reloadzsh="source ~/.zshrc"
+
+# Programs
+alias djm="python manage.py"
+alias cloud="~/work/cloudcli/cloudcli"
+alias cloud2="~/software/cloudcli/cloudcli2"
+alias r="rails"
+alias tar="gnutar"
+alias cleanssh="echo \"\" > ~/.ssh/known_hosts"
+alias ack="ag"
+
+# Bundle
+alias be="bundle exec"
+alias bi="bundle install"
+alias bc="bundle check"
+
+# SSH -D
+alias hkvpn="echo Connect to HK VPN; ssh -NvD 127.0.0.1:7070 hkvpn; echo Connection closed"
+alias usvpn="echo Connect to US VPN; ssh -NvD 127.0.0.1:7070 ccvpn; echo Connection closed"
+
+# Git alias
+alias gco="git checkout"
+alias gst="git status"
+alias gcp="git cherry-pick"
+alias gcommit="git commit"
+alias gadd="git add"
+alias gpull="git pull"
+alias gpush="git push"
+alias gbranch="git branch"
+alias glog="git log"
+alias glg="git lg"
+alias gmerge="git merge"
+alias gdiff="git diff"
+alias gremote="git remote"
+# End Git alias
+
+# File type alias
+alias -s py=mvim
+alias -s rb=mvim
+alias -s js=mvim
+alias -s css=mvim
+alias -s html=mvim
+alias -s haml=mvim
+alias -s txt=mate
+# End file type alias
